8ce73a735ffbf0c8ce73c53c5db164fe684d4b89
Mark Stephenson <mark2000stephenson@gmail.com>
2021-08-29 23:58:50 -0700

    More figures

--- doppler_sizing.m
+++ doppler_sizing.m
@@ -7,9 +7,42 @@ f_sounds = linspace(0,2000,100)'; % Hz
 
 f_shift = f_sounds .* abs(1 - v_sound ./ (v_sound + speeds));
 [X,Y] = meshgrid(speeds,f_sounds);
-figure
+figure("Name", "shiftsize")
+subplot(1,2,1)
 contourf(X, Y, f_shift, 20)
 c = colorbar;
-c.Label.String = "Max $f_{shift}$ [Hz]"
+c.Label.String = "$f_{\textrm{shift}}$ [Hz]";
+c.Label.Interpreter = "latex";
+c.TickLabelInterpreter = "latex";
 xlabel("$v_d$ [m/s]")
 ylabel("$f_d$ [Hz]")
+
+subplot(1,2,2)
+fd = [500,1000];
+vd = [7.5,15];
+theta = linspace(0,90);
+for f = fd
+    for v = vd
+        f_shift = f .* abs(1 - v_sound ./ (v_sound + v.*cosd(theta)));
+        plot(theta, 1./(f_shift(1)-f_shift), "DisplayName", "$v_d$ = " + v + "$\frac{\mathrm{m}}{\mathrm{s}}$; $f_d$ = " + f + " Hz")
+        hold on
+    end
+end
+xlim([0,90])
+xlabel("$\theta$ [deg]")
+ylim([0,2])
+ylabel("$t_m$ [s]")
+l = legend("Location","northeast");
+l.Position(1) = l.Position(1) + .2;
+saveNiceFigure(gcf,[6.5,2.5],"png")
+
+
+% labs = c.TickLabels;
+% for i = 1:length(labs)
+%     f = str2num(labs{i});
+%     tm = 1/f
+%     c.TickLabels{i} = f + " Hz; $t_m$ = " + num2str(tm,"%.3f") + " s";
+% end
+% 
+% ax = gca;
+% ax.Position(3) = ax.Position(3) - 0.1;
---
+++ sampling_attenuation.m
@@ -0,0 +1,20 @@
+figure("Name", "attenuation")
+x = linspace(-5,5,200);
+msize = 50
+
+colors = colororder
+
+gauss = exp(-x.^2);
+plot(x,gauss,"Color",colors(1,:),'DisplayName',"Gaussian")
+hold on
+plot(x,movmean(gauss,msize),'--',"Color",colors(1,:),'DisplayName',"Gaussian (avg)");
+
+sigmoid = 1./(1+exp(-x));
+plot(x, sigmoid,"Color",colors(2,:),'DisplayName',"Sigmoid")
+plot(x,movmean(sigmoid,msize),'--',"Color",colors(2,:),'DisplayName',"Sigmoid (avg)");
+
+legend("Location",'northwest')
+xlabel("$x$")
+ylabel("$y$")
+
+saveNiceFigure(gcf,[4,2.5],"png")
